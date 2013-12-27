5f7457b8213444f24d771e3f443c1aac2b1fe04f
Kenny Bastani <kb@socialmoon.com>
2013-12-27 07:29:34 -0800

    Updated Movie Page
    Updated the generic panels of the item page to movie UI components.
    Using Cast Away as template before data integration.

--- assets/js/application.js
+++ assets/js/application.js
@@ -22,4 +22,14 @@ $(document).ready(function($) {
 		[1600, 15]
 		]
 	});
+
+	var owlCast = $("#owl-demo-cast");
+
+	owlCast.owlCarousel({
+		items : 6,
+		itemsDesktop : [1199,6],
+		itemsDesktopSmall : [980,4],
+		itemsTablet: [768,3],
+		itemsMobile: [479, 2]
+	});
 });
