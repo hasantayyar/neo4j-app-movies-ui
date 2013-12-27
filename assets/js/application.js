8be6ff7f3acde0807663bf3e3fa27e5acf09ecff
Kenny Bastani <kb@socialmoon.com>
2013-12-26 23:27:08 -0800

    Customized Template, Added Top Carousel
    Customized the bootstrap template to Neo4j movies spec. Added top
    carousel with 3 items.

---
+++ assets/js/application.js
@@ -0,0 +1,10 @@
+// application.js
+
+$(document).ready(function($) {
+	$("#owl-example").owlCarousel({
+		items : 3,
+		itemsDesktop : [1199,3],
+		itemsDesktopSmall : [980,3],
+		itemsTablet: [768,2]
+	});
+});
