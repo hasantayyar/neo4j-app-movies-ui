69abede4a837fb4eea898edef6d7d66764f4b9dc
Kenny Bastani <kb@socialmoon.com>
2013-12-27 00:06:04 -0800

    Added Category Containers
    Added category containers 1-3 per the spec

--- assets/js/application.js
+++ assets/js/application.js
@@ -7,4 +7,19 @@ $(document).ready(function($) {
 		itemsDesktopSmall : [980,3],
 		itemsTablet: [768,2]
 	});
+
+	var owl = $("#owl-demo-1, #owl-demo-2, #owl-demo-3");
+ 
+	owl.owlCarousel({
+		itemsCustom : [
+		[0, 2],
+		[450, 4],
+		[600, 7],
+		[700, 9],
+		[1000, 10],
+		[1200, 12],
+		[1400, 13],
+		[1600, 15]
+		]
+	});
 });
