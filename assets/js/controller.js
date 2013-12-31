1e7f46247e0d326aa64923d1306d182e9b486d30
Kenny Bastani <kb@socialmoon.com>
2013-12-30 20:12:18 -0800

    Added Loader Animation For All REST Calls
    Added loader animation via Angular.js intercept,

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -2,7 +2,40 @@
 
 /* Controllers */
 
-var contentappControllers = angular.module('contentappControllers', []);
+angular.module('SharedServices', [])
+    .config(function ($httpProvider) {
+        $httpProvider.responseInterceptors.push('myHttpInterceptor');
+        var spinnerFunction = function (data, headersGetter) {
+            // todo start the spinner here
+            //alert('start spinner');
+            $('#mydiv').show();
+            return data;
+        };
+        $httpProvider.defaults.transformRequest.push(spinnerFunction);
+    })
+// register the interceptor as a service, intercepts ALL angular ajax http calls
+    .factory('myHttpInterceptor', function ($q, $window) {
+        return function (promise) {
+            return promise.then(function (response) {
+                // do something on success
+                // todo hide the spinner
+                //alert('stop spinner');
+                $('#mydiv').hide();
+                $('.hidden-content').removeClass('hidden-content');
+                return response;
+
+            }, function (response) {
+                // do something on error
+                // todo hide the spinner
+                //alert('stop spinner');
+                $('#mydiv').hide();
+                $('.hidden-content').removeClass('hidden-content');
+                return $q.reject(response);
+            });
+        };
+    });
+
+var contentappControllers = angular.module('contentappControllers', ['SharedServices']);
 
 contentApp.directive('carousel', function() {
 	var res = {
