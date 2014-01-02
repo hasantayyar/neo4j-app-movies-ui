cdcb675cdef1c2457d7e666dc2267305f6e31d92
Kenny Bastani <kb@socialmoon.com>
2014-01-02 12:12:54 -0800

    Updating dependencies

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
+var util = require('util'),
+    connect = require('connect'),
+    port = 3030;
 
-//var __dirname = "/";
-
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(8080, function() {
-  console.log('Express server listening on port ' + 8080);
-});
+connect.createServer(connect.static(__dirname)).listen(port);
+util.puts('Listening on ' + port + '...');
+util.puts('Press Ctrl + C to stop.');
