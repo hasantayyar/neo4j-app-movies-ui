c6b088cffb7a2b279d49ab47254993672bc8c994
Kenny Bastani <kb@socialmoon.com>
2014-01-02 12:37:26 -0800

    Updating dependencies

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-var util = require('util'),
-    connect = require('connect'),
-    port = 3030;
+var express = require('express');
+var app = express();
+app.use(express.logger());
 
-connect.createServer(connect.static(__dirname)).listen(port);
-util.puts('Listening on ' + port + '...');
-util.puts('Press Ctrl + C to stop.');
+// var http_handler = express.static(__dirname + '/');
+
+app.configure(function(){
+  app.use('/assets', express.static(__dirname + '/assets'));
+  app.use(express.static(__dirname + '/'));
+});
+
+// app.get('/', function(req, res, next) {
+//   // if (req.url === '/docs') { // express static barfs on root url w/o trailing slash
+//   //   res.writeHead(302, { 'Location' : req.url + '/' });
+//   //   res.end();
+//   //   return;
+//   // }
+//   // take off leading /docs so that connect locates file correctly
+//   // req.url = req.url.substr('/docs'.length);
+//   return http_handler(req, res, next);
+// });
+
+// // redirect to /docs
+// app.get('/', function(req, res) {
+//   res.redirect('./docs');
+// });
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
